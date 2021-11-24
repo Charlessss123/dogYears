36f4bb8aa37b651ae57df5fa48ffbbaf86ff3499
Charlessss123 <91907318+Charlessss123@users.noreply.github.com>
2021-11-24 05:21:33 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+let myAge = 24; // Human age
+
+let earlyYears = 2; // early years
+
+ earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+laterYears *= 4 //number of dog years
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'Charles'.toLowerCase(); //my name in lower case letters
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} old in dog years`)
